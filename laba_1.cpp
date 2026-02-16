aa34cc20ba8e8c3c15d73d944e842fb4c0838353
Gra1mond <pronin1979dmitriy@gmail.com>
2026-02-16 23:05:54 +0300

    Second test

--- laba_1.cpp
+++ laba_1.cpp
@@ -1,28 +1,28 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 #include "datelib.h"
 
 int main() {
-    int year[Constant::kAmountDate];  
-    int month[Constant::kAmountDate];  
-    int day[Constant::kAmountDate];    
-    int out[Constant::kLenDate];       
-    
+    int year[Constant::kAmountDate];
+    int month[Constant::kAmountDate];
+    int day[Constant::kAmountDate];
+    int out[Constant::kLenDate];
+
     std::cout << "Введите год для двух дат: ";
     dateInput(year);
-    
+
     std::cout << "Введите месяц для двух дат: ";
     dateInput(month);
-    
+
     std::cout << "Введите день для двух дат: ";
     dateInput(day);
-    
+
     dateDifference(year, month, day, out);
-    
+
     std::cout << "Разница между датами:" << std::endl;
     std::cout << "Лет: " << abs(out[Constant::kIndexFirstDate]) << std::endl;
     std::cout << "Месяцев: " << abs(out[Constant::kIndexSecondDate]) << std::endl;
     std::cout << "Дней: " << abs(out[Constant::kIndexThirdDate]) << std::endl;
-    
+
     return 0;
 }

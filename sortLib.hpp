8f539208a3457d05973b61c15b134db8da422ea4
Gra1mond <pronin1979dmitriy@gmail.com>
2026-03-18 12:55:11 +0300

    update

--- sortLib.hpp
+++ sortLib.hpp
@@ -1,11 +1,12 @@
 #pragma once
-
 #include "wine_library.hpp"
-
-bool isEmptyMassive(const int WineLibraryCount);
-void loadFromFile(wineLibrary*& testarray, int* wineLibraryCount);
-void loadToFile(wineLibrary*& testarray, int* wineLibraryCount);
-void addNewobject(wineLibrary*& testarray, int* currentPos, int* wineLibraryCount);
-void deleteObject(wineLibrary*& testarray, int* wineLibraryCount);
-void searchObject(wineLibrary*& testarray, int* wineLibraryCount);
-void redactObject(wineLibrary*& testarray, int* wineLibraryCount);
+enum class SortSelect : int {
+    Mark = 1,
+    Date = 2,
+    Count = 3,
+    Price = 4
+};
+namespace SortConstant {
+int const kBufferAccess = 1;
+}
+void sortWineLibrary(wineLibrary*& testarray, int* wineLibraryCount);
